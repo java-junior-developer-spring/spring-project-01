63f1beedd1d5d12984808f34dd9e5f2a51c20a27
dasha <jjg-lessons@gmail.com>
2024-02-09 08:42:54 +0300

    thymeleaf fragments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,10 @@ VALUES ('01-01-2023', false, 'Описание...', 'stilllife', 'picture01.png'
 INSERT INTO tb_picture (created_at, in_archive, description, genre_url, image_path, name)
 VALUES ('01-04-2023', false, 'Описание...', 'stilllife', 'picture02.png', 'Картина №2');
 
+INSERT INTO tb_boxes (height, length, width) VALUES (12, 45, 78);
+INSERT INTO tb_boxes (height, length, width) VALUES (122, 56, 200);
+INSERT INTO tb_boxes (height, length, width) VALUES (20, 100, 80);
+INSERT INTO tb_boxes (height, length, width) VALUES (20, 40, 60);
 
 
 -- schema.sql запросы на создание объектов БД (например, таблиц)
