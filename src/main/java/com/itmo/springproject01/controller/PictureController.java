7568ffb569ed4f653e376d12d40e9c8ebde6e041
dasha <jjg-lessons@gmail.com>
2024-02-13 22:36:57 +0300

    picture: контроллер и сервис

--- src/main/java/com/itmo/springproject01/controller/PictureController.java
+++ src/main/java/com/itmo/springproject01/controller/PictureController.java
@@ -1,30 +1,37 @@
 package com.itmo.springproject01.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itmo.springproject01.entity.Genre;
 import com.itmo.springproject01.entity.Picture;
 import com.itmo.springproject01.exception.ShopException;
 import com.itmo.springproject01.helper.PictureParamEditor;
+import com.itmo.springproject01.service.GenreService;
 import com.itmo.springproject01.service.PictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller // @Controller + @ResponseBody
 @RequestMapping("/picture")
 public class PictureController {
     private final PictureService pictureService;
+    private final GenreService genreService;
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public PictureController(PictureService pictureService, ObjectMapper objectMapper) {
+    public PictureController(PictureService pictureService, GenreService genreService, ObjectMapper objectMapper) {
         this.pictureService = pictureService;
+        this.genreService = genreService;
         this.objectMapper = objectMapper;
     }
 
@@ -51,14 +58,19 @@ public class PictureController {
     }
     */
 
-
-    /*@ResponseBody
+    @ResponseBody
     @PostMapping
     public ResponseEntity<Void> addPicture(@RequestPart Picture picture,
-                                           @RequestPart MultipartFile image){
-        // /uploads/ngjegh3598jfq.png
-        // picture.setImagePath("/uploads/ngjegh3598jfq.png");
-    }*/
+                                           @RequestPart String genreUrl,
+                                           @RequestPart MultipartFile image) {
+        try {
+            int pictureId = pictureService.saveNewPicture(picture, genreUrl, image);
+            URI location = URI.create("http://127.0.0.1:8080/picture/" + pictureId);
+            return ResponseEntity.created(location).build();
+        } catch (ShopException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 
     // import org.springframework.ui.Model;
     @GetMapping("/{pictureId}")
@@ -66,6 +78,8 @@ public class PictureController {
         try {
             Picture picture = pictureService.getPictureById(pictureId);
             model.addAttribute("pictureInfo", picture);
+            Iterable<Genre> genreList = genreService.getAllGenres();
+            model.addAttribute("genres", genreList);
             // имя html файла, который находится в папке templates
             return "picture";
         } catch (ShopException e) {
@@ -76,14 +90,25 @@ public class PictureController {
     @GetMapping("/genre/{genreUrl}")
     public String getPictureById(@PathVariable String genreUrl, Model model) {
         List<Picture> picturesList = pictureService.getPicturesByGenre(genreUrl);
+        Iterable<Genre> genreList = genreService.getAllGenres();
         model.addAttribute("pictures", picturesList);
+        model.addAttribute("genres", genreList);
         // имя html файла, который находится в папке templates
         return "pictures";
     }
 
-    @GetMapping
+    @GetMapping("/not_found")
+    public String notFound() {
+        return "not_found";
+    }
+
+
+    @GetMapping // ?page=0&size=2
     public String getPictures(@RequestParam int page, @RequestParam int size, Model model) {
         List<Picture> picturesList = pictureService.getPictures(page, size);
+        /*if (picturesList == null || picturesList.isEmpty()){
+            return "pictures_not_found"; // "redirect:picture/not_found";
+        }*/
         model.addAttribute("pictures", picturesList);
         // имя html файла, который находится в папке templates
         return "pictures";
--- src/main/java/com/itmo/springproject01/service/PictureService.java
+++ src/main/java/com/itmo/springproject01/service/PictureService.java
@@ -1,7 +1,9 @@
 package com.itmo.springproject01.service;
 
+import com.itmo.springproject01.entity.Genre;
 import com.itmo.springproject01.entity.Picture;
 import com.itmo.springproject01.exception.ShopException;
+import com.itmo.springproject01.repository.GenreRepository;
 import com.itmo.springproject01.repository.PictureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,16 +11,23 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PictureService {
     private final PictureRepository pictureRepository;
+    private final GenreRepository genreRepository;
 
     @Autowired
-    public PictureService(PictureRepository pictureRepository) {
+    public PictureService(PictureRepository pictureRepository, GenreRepository genreRepository) {
         this.pictureRepository = pictureRepository;
+        this.genreRepository = genreRepository;
     }
 
     public Picture getPictureById(int pictureId) throws ShopException {
@@ -45,5 +54,28 @@ public class PictureService {
         // limit + offset + sort
         Pageable pageable = PageRequest.of(page, size, Sort.by("name"));
         return pictureRepository.findAll(pageable);
+    }
+
+    // name, description, createdAt, ?? imagePath ??
+    // genreUrl
+    // изображение
+    public int saveNewPicture(Picture picture, String genreUrl, MultipartFile image)
+            throws ShopException {
+        Genre genre = genreRepository.findById(genreUrl)
+                .orElseThrow(()->new ShopException("Жанра не существует"));
+        picture.setImagePath(saveFile(image));
+        picture.setGenre(genre);
+        return pictureRepository.save(picture).getId();
+    }
+
+    private String saveFile(MultipartFile file) throws ShopException {
+        // TODO:: проверка на тип и размер файла
+        String fileName = "/path_to_file_storage/" + UUID.randomUUID();
+        try {
+            Files.write(Path.of(fileName), file.getBytes());
+            return fileName;
+        } catch (IOException e) {
+            throw new ShopException("Проблема загрузки файла " + e.getMessage());
+        }
     }
 }
