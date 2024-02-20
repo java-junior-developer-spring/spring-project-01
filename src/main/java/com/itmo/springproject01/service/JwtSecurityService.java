34d08bafd1ed89cea7b794837f3a7cb6aa690a91
java-junior-developer-spring <156890926+java-junior-developer-spring@users.noreply.github.com>
2024-02-20 19:34:51 +0300

    Create JwtSecurityService.java

---
+++ src/main/java/com/itmo/springproject01/service/JwtSecurityService.java
@@ -0,0 +1,110 @@
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jose.proc.JWEDecryptionKeySelector;
+import com.nimbusds.jose.proc.JWEKeySelector;
+import com.nimbusds.jose.proc.SimpleSecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.UUID;
+
+
+@Service
+public class JwtSecurityService {
+
+    private final WebApplicationContext webApplicationContext;
+
+    private String jwtSecret;
+    private String jwtRefreshSecret;
+    private long jwtSecretExpiration;
+
+
+    @Autowired
+    public JwtSecurityService(WebApplicationContext webApplicationContext) {
+        this.webApplicationContext = webApplicationContext;
+    }
+
+    private JWEHeader getHeader() {
+        return new JWEHeader(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256);
+    }
+
+    private Payload getPayload(String subject, String name) {
+        JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                .subject(subject)
+                .claim("name", name)
+                .issueTime(new Date())
+                .expirationTime(new Date(System.currentTimeMillis() + jwtSecretExpiration))
+                .build();
+
+        return new Payload(claims.toJSONObject());
+    }
+
+    private void encrypt(JWEObject jweObject) throws JOSEException {
+        jweObject.encrypt((JWEEncrypter) webApplicationContext.getBean("JWEEncrypter"));
+//        jweObject.encrypt((JWEEncrypter) webApplicationContext.getBean("JWEEncrypter"));
+    }
+
+    public String generateToken(CustomerUserDetails customerUserDetails) throws JOSEException {
+        JWEObject jweObject = new JWEObject(
+                getHeader(),
+                getPayload(
+                        customerUserDetails.getUsername(),
+                        customerUserDetails.getCustomer().getName()));
+
+        encrypt(jweObject);
+        return jweObject.serialize();
+    }
+
+    @Bean("JWEEncrypter")
+    public JWEEncrypter encrypter() throws KeyLengthException {
+        return new DirectEncrypter(jwtSecret.getBytes());
+    }
+
+    @Bean("JWTProcessor")
+    public ConfigurableJWTProcessor<SimpleSecurityContext> jwtProcessor() {
+        ConfigurableJWTProcessor<SimpleSecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+        JWKSource<SimpleSecurityContext> jweKeySource = new ImmutableSecret<>(jwtSecret.getBytes());
+        JWEKeySelector<SimpleSecurityContext> jweKeySelector =
+                new JWEDecryptionKeySelector<>(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256, jweKeySource);
+        jwtProcessor.setJWEKeySelector(jweKeySelector);
+        return jwtProcessor;
+    }
+
+    public String generateRefreshToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    private JWTClaimsSet extractClaims(String token) throws BadJOSEException, ParseException, JOSEException {
+        ConfigurableJWTProcessor<SimpleSecurityContext> jwtProcessor =
+                (ConfigurableJWTProcessor<SimpleSecurityContext>) webApplicationContext.getBean("JWTProcessor");
+        return jwtProcessor.process(token, null);
+    }
+
+    public String getSubject(String token) throws BadJOSEException, ParseException, JOSEException {
+        JWTClaimsSet claims = extractClaims(token);
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) throws BadJOSEException, ParseException, JOSEException {
+        String userName = getSubject(token);
+        Date expiration = extractClaims(token).getExpirationTime();
+        return userName.equals(userDetails.getUsername()) && expiration.after(new Date());
+    }
+
+    public String getStringClaimsByKey(String token, String claimsKey) throws BadJOSEException, ParseException, JOSEException {
+        JWTClaimsSet claims = extractClaims(token);
+        return claims.getStringClaim(claimsKey);
+    }
+
+}
